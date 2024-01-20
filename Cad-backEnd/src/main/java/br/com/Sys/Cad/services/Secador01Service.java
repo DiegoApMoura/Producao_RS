24693b6969eb8a40ada9637d3c8bedde72b4d9c7
Diego Moura <diego_apmoura@hotmail.com>
2024-01-20 17:46:12 -0300

    database seeding

--- Cad-backEnd/src/main/java/br/com/Sys/Cad/services/Secador01Service.java
+++ Cad-backEnd/src/main/java/br/com/Sys/Cad/services/Secador01Service.java
@@ -4,6 +4,7 @@ import br.com.Sys.Cad.Entities.Secador01;
 import br.com.Sys.Cad.repositories.Secador01Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class Secador01Service {
 
     @Autowired
     private Secador01Repository repository;
+    @Transactional(readOnly = true)
     public List<Secador01> findAll(){
         return repository.findAll();
     }
